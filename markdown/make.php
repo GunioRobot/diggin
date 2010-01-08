752229c95b5ed41ad34fd18c0af49fcbc40e9225
kazusuke sasezaki <sasezaki+github@gmail.com>
2010-01-08 15:26:10 +0900

    make.php update

--- markdown/make.php
+++ markdown/make.php
@@ -1,20 +1,34 @@
 <?php
-$library = 'http://code.google.com/p/diggin/source/browse/standard/trunk/library';
-if (!$argv[1]) die('Need markdownfile');
-$markdownfile = $argv[1];
-if (!preg_match('/.markdown$/', $markdownfile)) die('not markdown suffix');
-
-$title = 'Diggin リファレンス';
-if ($markdownfile != 'diggin.index.markdown') {
-    $title = preg_replace('/\.markdown$/i', '',$markdownfile);
-    $title = explode('.', $title);
-    foreach ($title as $k => $t) $title[$k]  = ucfirst($t);
-    $title = implode('_', $title);
-}
+// Note for publish 
+// find ./ -name '*.html' | xargs sed -i 's/\.markdown\"/.html"/g'
 
 require 'lib/markdown.php';
+$library = 'http://code.google.com/p/diggin/source/browse/standard/trunk/library';
+if (!$argv[1]) die('Need markdownfile');
 
-$text = Markdown(file_get_contents($argv[1]));
+if(is_dir($argv[1])) {
+    foreach(new DirectoryIterator($argv[1]) as $f) {
+        if (!is_file($f)) continue;
+        makeFile($f);
+    }
+} elseif (is_file($argv[1])) {
+    makeFile($argv[1]);
+} else {
+    echo 'nothing to do';
+}
+
+function makeFile($markdownfile) { 
+    if (!preg_match('/.markdown$/', $markdownfile)) return; //die('not markdown suffix');
+
+    $title = 'Diggin リファレンス';
+    if ($markdownfile != 'diggin.index.markdown') {
+        $title = preg_replace('/\.markdown$/i', '',$markdownfile);
+        $title = explode('.', $title);
+        foreach ($title as $k => $t) $title[$k]  = ucfirst($t);
+        $title = implode('_', $title);
+    }
+
+$text = Markdown(file_get_contents($markdownfile));
 
 $head = <<<EOF
 <html>
@@ -45,7 +59,8 @@ EOF;
 
 $markdownfile = realpath($markdownfile);
 
-$markdownfile = dirname($markdownfile).'/html/'.pathinfo($markdownfile, PATHINFO_FILENAME).'.markdown';
+$markdownfile = dirname($markdownfile).'/html/'.pathinfo($markdownfile, PATHINFO_FILENAME).'.html';
 var_dump($markdownfile);
 
 file_put_contents($markdownfile, $head.$text.$foot);
+}
